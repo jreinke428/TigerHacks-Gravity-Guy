00f01fe4c36263b256b9fe809d7d71b161238e0d
jreinke428 <93751082+jreinke428@users.noreply.github.com>
2021-12-02 16:50:24 -0600

    made ui look better

--- Gravity Guy/Assets/Scripts/UIController.cs
+++ Gravity Guy/Assets/Scripts/UIController.cs
@@ -12,6 +12,12 @@ public class UIController : MonoBehaviour
     public LevelData[] levels;
     public LevelData curLevel;
     public GameData data;
+    public GameObject pullWave;
+    public GameObject pushWave;
+    public GameObject gravity;
+    public GameObject pullBorder;
+    public GameObject pushBorder;
+    public GameObject gravityBorder;
     public int levelNum;
     public int attempts;
     public int pushNums;
@@ -25,11 +31,15 @@ public class UIController : MonoBehaviour
         pushNums = curLevel.pushNum;
         pullNums = curLevel.pullNum;
         levelNumText.GetComponent<TextMeshProUGUI>().text = levelNum.ToString();
+        gravity.GetComponent<Animator>().SetBool("gravSelected", true);
+        pushBorder.SetActive(false);
+        pullBorder.SetActive(false);
     }
 
     private void Update()
     {
         UpdateText();
+        CheckInputs();
     }
 
     void UpdateText()
@@ -37,5 +47,36 @@ public class UIController : MonoBehaviour
         attemptsText.GetComponent<TextMeshProUGUI>().text = attempts.ToString();
         pushNumText.GetComponent<TextMeshProUGUI>().text = pushNums.ToString();
         pullNumText.GetComponent<TextMeshProUGUI>().text = pullNums.ToString();
+    }
+
+    public void CheckInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            pullWave.GetComponent<Animator>().SetBool("pullSelected", false);
+            pushWave.GetComponent<Animator>().SetBool("pushSelected", true);
+            gravity.GetComponent<Animator>().SetBool("gravSelected", false);
+            pushBorder.SetActive(true);
+            pullBorder.SetActive(false);
+            gravityBorder.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            pullWave.GetComponent<Animator>().SetBool("pullSelected", true);
+            pushWave.GetComponent<Animator>().SetBool("pushSelected", false);
+            gravity.GetComponent<Animator>().SetBool("gravSelected", false);
+            pushBorder.SetActive(false);
+            pullBorder.SetActive(true);
+            gravityBorder.SetActive(false);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            pullWave.GetComponent<Animator>().SetBool("pullSelected", false);
+            pushWave.GetComponent<Animator>().SetBool("pushSelected", false);
+            gravity.GetComponent<Animator>().SetBool("gravSelected", true);
+            pushBorder.SetActive(false);
+            pullBorder.SetActive(false);
+            gravityBorder.SetActive(true);
+        }
     }
 }
